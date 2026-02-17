2b5cef3b666db3be647bef983a6b57f66300b2a6
CyanideData2 <danielhurtadolabandeira@gmail.com>
2026-02-17 19:26:50 +0100

    Implemented basic functionality, as well as a solid autosave function and a dubious Real Time onChange

---
+++ TODO.md
@@ -0,0 +1,5 @@
+== Quickfixes to change
+- file is required tho it ideally wouldn't
+
+== Possible performance enhancers
+- OnChange set changed:bool = true which sets to false after writting
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 
-import { ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
+import { AppState, BinaryFiles, ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types";
 import {
     Excalidraw,
     exportToSvg,
@@ -12,34 +12,119 @@ import {
 
 import "@excalidraw/excalidraw/index.css";
 import "./App.css";
+import { getMatches } from "@tauri-apps/plugin-cli";
+import { NonDeletedExcalidrawElement, Ordered } from "@excalidraw/excalidraw/element/types";
+const SVG_DOCUMENT_PREAMBLE = `<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+`;
 
+const framerate = 30;
+const frameDuration = 1000 / framerate;
 function App() {
     const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI>();
+    const [shouldSave, setShouldSave] = useState(true);
+    const [renderOnChange, setRenderOnChange] = useState(false);
 
+
+    //Utils
+    let saveSVGToFile = async (elements: readonly Ordered<NonDeletedExcalidrawElement>[], state: AppState, files: BinaryFiles) => {
+        console.info("Excalidraw API initialized");
+        console.info("Attempting to save file")
+        const svg = await exportToSvg({
+            elements: elements,
+            appState: {
+                ...state,
+                exportBackground: false,
+                exportWithDarkMode: true,
+                exportEmbedScene: true
+            },
+            files: files,
+        })
+        const code_status = await invoke<number>("save_svg", { svg: (SVG_DOCUMENT_PREAMBLE + svg.outerHTML) });
+        if (code_status != 0) {
+            console.warn("Couldn't save SVG to file");
+        }
+        return code_status
+    };
+
+    let trySaveSVG = () => {
+        if (shouldSave == false) {
+            return
+        }
+        if (!excalidrawAPI) {
+            console.warn("Excalidraw API not initialized yet");
+            return;
+        }
+        setShouldSave(false);
+        const elements = excalidrawAPI.getSceneElements();
+        const state = excalidrawAPI.getAppState();
+        const files = excalidrawAPI.getFiles();
+        saveSVGToFile(elements, state, files).then(() => setShouldSave(true))
+    }
+    let realTimeSaveSVG = (elements: readonly Ordered<NonDeletedExcalidrawElement>[], state: AppState, files: BinaryFiles) => {
+        if (shouldSave == false) {
+            return
+        }
+        setShouldSave(false);
+        saveSVGToFile(elements, state, files).then(() => {
+            setTimeout(() => setShouldSave(true), frameDuration)
+        })
+    }
+
+    //Initialize
     useEffect(() => {
+        //Setup saving to file
+        getMatches().then(matches => {
+            if (matches.args.autosave?.value) {
+                console.info("Saving to file automatically every 30s")
+                setInterval(() => {
+                    trySaveSVG();
+                }, 30000);
+            } else if (matches.args.rtsave?.value) {
+                console.info("Saving to file in real time")
+                setRenderOnChange(true);
+            }
+        })
+        // Load file from backend
         invoke<string>('get_initial_svg').then((svgContent) => {
             if (!excalidrawAPI) {
                 console.warn("Excalidraw API not initialized yet");
                 return;
             }
-            console.log("Loading initial SVG content");
+            console.info("Loading initial SVG content");
             loadFromBlob(
                 new Blob([svgContent], { type: MIME_TYPES.svg }),
                 excalidrawAPI.getAppState(),
                 null
             ).then((contents) => {
-                excalidrawAPI.updateScene(contents);
+                excalidrawAPI.updateScene({
+                    ...contents,
+                    appState: { ...contents.appState, isLoading: false },
+                });
                 if (contents.files) {
                     excalidrawAPI.addFiles(Object.values(contents.files));
                 }
+            }).catch(error => {
+                console.warn(error);
+                console.info("Starting empty scene");
+                excalidrawAPI.resetScene({ resetLoadingState: true });
             });
         });
     }, [excalidrawAPI]);
-
+    useEffect(() => {
+        document.addEventListener("keydown", (e) => {
+            if (e.ctrlKey && e.key == "s") {
+                e.preventDefault();
+                trySaveSVG();
+            }
+        })
+    }, [])
     return (
         <main className="container">
             <Excalidraw
+                handleKeyboardGlobally={true}
                 excalidrawAPI={(api) => setExcalidrawAPI(api)}
+                onChange={renderOnChange ? realTimeSaveSVG : undefined}
                 initialData={{
                     appState: {
                         theme: THEME.DARK,
@@ -47,15 +132,6 @@ function App() {
                     },
                 }}
                 theme={THEME.DARK}
-                onChange={(elements, state, files) => {
-                    exportToSvg({
-                        elements: elements,
-                        appState: { ...state, exportBackground: false, exportWithDarkMode: true, exportEmbedScene: true },
-                        files: files,
-                    }).then((svg: SVGSVGElement) => {
-                        invoke<void>("save_svg", { svg: svg.outerHTML });
-                    });
-                }}
             />
         </main>
     );
